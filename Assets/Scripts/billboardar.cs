79bb6f504c1e0a71a7b3fadde6de5cfc0f27c3f1
David Pollack <david@da3.net>
2017-12-03 07:57:55 +0100

    UnityAR

---
+++ Assets/Scripts/billboardar.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Lean.Touch
+{
+	public class billboardar : MonoBehaviour {
+
+		SpriteRenderer rend;
+
+		protected virtual void OnEnable()
+		{
+			rend = this.gameObject.GetComponent<SpriteRenderer> ();
+			// Hook into the events we need
+			LeanTouch.OnGesture     += OnGesture;
+		}
+
+		protected virtual void OnDisable()
+		{
+			// Unhook the events
+			LeanTouch.OnGesture     -= OnGesture;
+		}
+
+		public void OnGesture(List<LeanFinger> fingers)
+		{
+			rend.enabled = false;
+			rend.color = new Color(1f,1f,1f,0f);
+		}
+			
+	}
+}
---
+++ Assets/Scripts/buttonPopTest.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using Vuforia;
+
+public class buttonPopTest : MonoBehaviour, ITrackableEventHandler {
+
+	private TrackableBehaviour mTrackableBehaviour;
+
+	private bool mShowGUIButton = false;
+	private Rect mButtonRect = new Rect(400,200,250,100);
+
+	void Start () {
+		mTrackableBehaviour = GetComponent<TrackableBehaviour>();
+		if (mTrackableBehaviour)
+		{
+			mTrackableBehaviour.RegisterTrackableEventHandler(this);
+		}
+	}
+
+	public void OnTrackableStateChanged(
+		TrackableBehaviour.Status previousStatus,
+		TrackableBehaviour.Status newStatus)
+	{
+		if (newStatus == TrackableBehaviour.Status.DETECTED ||
+			newStatus == TrackableBehaviour.Status.TRACKED)
+		{
+			mShowGUIButton = true;
+		}
+		else
+		{
+			mShowGUIButton = false;
+		}
+	}
+
+	void OnGUI() {
+		if (mShowGUIButton) {
+			// draw the GUI button
+//			if (GUI.Button(mButtonRect, "Hello")) {
+//				// do something on button click 
+//			}
+		}
+	}
+}
